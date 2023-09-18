057e53bb467de69ae2bd8b03d364498833498c0e
hanae <hanae.manekoudi@gmail.com>
2023-09-18 14:14:40 +0100

    popular

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+/*=============== SWIPER POPULAR ===============*/
+var swiperPopular = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor:true,
+    centeredSlides:true,
+    slidesPerView:'auto',
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+/*=============== VALUE ACCORDION ===============*/
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
