6f297f9e93dff9e10986366822ecce80d7db5b27
hanae <hanae.manekoudi@gmail.com>
2023-09-19 12:27:37 +0100

    contatc

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,17 +2,18 @@
 
 
 /*=============== SWIPER POPULAR ===============*/
-var swiperPopular = new Swiper(".popular__container", {
-    spaceBetween: 32,
-    grabCursor:true,
-    centeredSlides:true,
-    slidesPerView:'auto',
-    loop:true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+const stgSwipper = {
+  spaceBetween: 32,
+  grabCursor:true,
+  centeredSlides:true,
+  slidesPerView:'auto',
+  loop:true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+}
+var swiperPopular = new Swiper(".popular__container", stgSwipper);
 
 /*=============== VALUE ACCORDION ===============*/
 
