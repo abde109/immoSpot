50212886b9c7da791f38171b397535156a912ff2
abde109 <abderrahimkhadri2@gmail.com>
2023-09-19 15:33:58 +0100

    merge footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,9 @@
 
 function scrollHeader() {
   const header = document.getElementById('header');
-  if(this.scollY >= 50) header.classList.add('scroll-header');
+  if(this.scrollY >= 50){ 
+    header.classList.add('scroll-header'); 
+  } else { header.classList.remove('scroll-header'); }
 }
 
 window.addEventListener('scroll', scrollHeader);
