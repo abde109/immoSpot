32e8ccd62cd72bdc3c04721493ed5047b05f762f
hanae <hanae.manekoudi@gmail.com>
2023-09-20 14:22:28 +0100

    logo color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,4 @@
-/*=============== CHANGE BACKGROUND HEADER ===============*/
 
-function scrollHeader() {
-  const header = document.getElementById('header');
-  if(this.scrollY >= 50){ 
-    header.classList.add('scroll-header'); 
-  } else { header.classList.remove('scroll-header'); }
-}
-
-window.addEventListener('scroll', scrollHeader);
 
 /*=============== SWIPER POPULAR ===============*/
 const stgSwipper = {
@@ -119,3 +110,15 @@ const toggleItem = (item) => {
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+
+function scrollHeader() {
+  const header = document.getElementById('header');
+  const headerLogo = document.querySelector('.logo__header');
+  if(this.scrollY >= 50){ 
+    header.classList.add('scroll-header'); 
+  } else { header.classList.remove('scroll-header'); }
+}
+
+window.addEventListener('scroll', scrollHeader);
